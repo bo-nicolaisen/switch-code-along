f3cebc3dd52b7ebc37d46c56bd763e1af69e505a
bo-nicolaisen <bni@techcollege.dk>
2024-02-26 13:30:58 +0100

    full MVC

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,6 @@
 // switch structure code-along
 
-let myValue = 35
+//let myValue = 35
 
 /* // standard switch med Default
 switch (myValue) {
@@ -77,37 +77,36 @@ switch (myValue) {
 
 
 
+GetCategoryData()
+
+
+
+
+function GetCategoryData() {
+    //console.log("fetch");
+    fetch('https://dummyjson.com/products/categories')
+
+        .then((result) => {
+            return result.json()
+        }
+        )
+
+        .then((json) => {
+            //console.log(json);
+            categorySorter(json)
+        });
+}
 
 
 
 
 
-let myCategories = ["smartphones",
-    "laptops",
-    "fragrances",
-    "skincare",
-    "groceries",
-    "home-decoration",
-    "furniture",
-    "tops",
-    "womens-dresses",
-    "womens-shoes",
-    "mens-shirts",
-    "mens-shoes",
-    "mens-watches",
-    "womens-watches",
-    "womens-bags",
-    "womens-jewellery",
-    "sunglasses",
-    "automotive",
-    "motorcycle",
-    "lighting"
-]
 
-categorySorter(myCategories)
 
+
+// controller 
 function categorySorter(categoriesToSort) {
-
+    console.log("sort");
 
     // hoved kategori arrays
     let myElectronics = []
@@ -122,7 +121,7 @@ function categorySorter(categoriesToSort) {
             case 'laptops':
             case 'lighting':
             case 'smartphones':
-                console.log('electronics');
+
                 myElectronics.push(category)
                 break;
 
@@ -139,16 +138,104 @@ function categorySorter(categoriesToSort) {
                 break;
 
             default:
-                console.log('misch');
+
                 myMisc.push(category)
                 break;
         }
 
     });
 
-    console.log(myElectronics);
+    /* console.log(myElectronics);
     console.log(myCosmetics);
     console.log(myVehicles);
-    console.log(myMisc);
+    console.log(myMisc); */
+
+    // build datastructure to view code
+    let myNavigationData = [
+        {
+            superCategoryname: 'Electronics',
+            subCategories: myElectronics
+        },
+        {
+            superCategoryname: 'Cosmetics',
+            subCategories: myCosmetics
+        },
+        {
+            superCategoryname: 'Vehicles',
+            subCategories: myVehicles
+        },
+        {
+            superCategoryname: 'misc',
+            subCategories: myMisc
+        }
+
+    ]
+
+    BuildNavigation(myNavigationData);
+
+}
+
+
+
+// view
+function BuildNavigation(myNavigationData) {
+
+    // hvor skal vi bygge navigation
+    let myNavElement = document.getElementById('navigation')
+
+    myNavigationData.forEach(superCatData => {
+
+        // ul from category array
+
+        let mySubCats = '<ul>'
+        superCatData.subCategories.forEach(subCatName => {
+            let myListElement = `<li><div class="navRollover"onClick="navCallback('${subCatName}')">${subCatName}</div></li>`
+            mySubCats += myListElement
+        });
+        mySubCats += '</ul>'
+
+        //console.log(mySubCats);
+        //console.log(superCat.superCategoryname);
+        let myCatHTML = `<div class="navCategories"><h3 class="navRollover" onClick="navCallback('${superCatData.superCategoryname}')">${superCatData.superCategoryname}</h3>
+        ${mySubCats}
+        </div>`
+        myNavElement.innerHTML += myCatHTML
+    });
+
+
+}
+
+
+/*
+<div class="navCategories">
+
+            <h3 class="navRollover" onClick="navCallback('category 1')">category 1</h3>
+            <ul>
+                <li>
+                    <div class="navRollover" onClick="navCallback('sub 1 of 1')">sub 1</div>
+                </li>
+                <li>
+                    <div class="navRollover" onClick="navCallback('sub 2 of 1')">sub 2</div>
+                </li>
+
+            </ul>
+        </div>
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+function navCallback(myItem) {
+    console.log(myItem);
 
 }
